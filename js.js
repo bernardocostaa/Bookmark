18b7bb1927b952f5998f93662b3866d3a56c87af
Bernardo Costa <bfvcc01@gmail.com>
2023-09-24 15:39:30 -0300

    ajuste

--- js.js
+++ js.js
@@ -17,3 +17,48 @@ function toggleModal(){
 }
 
 btnMobile.addEventListener('click',toggleModal)
+
+
+const btnEmail = document.querySelector('.btn-email')
+const areaInput = document.querySelector('.input-email')
+
+function checkEmail(event){
+    event.preventDefault()
+    const inputEmail = document.getElementById('inputEmail')
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const inputValue = inputEmail.value.trim()
+    const valideInput = emailRegex.test(inputEmail.value)
+    if(inputValue && valideInput){
+        removeErro()
+    }else{
+        showErro('Whoops, make sure it’s an email')
+    }
+}
+
+function showErro(text){
+    if(!document.querySelector('.area-input span')){
+        const areaErro = document.querySelector('.area-input')
+        areaInput.classList.add('error')
+        let spanErro = document.createElement('span')
+        let imgErro = document.createElement('img')
+        imgErro.setAttribute('src','./img/icon-error.svg')
+        spanErro.innerText = text
+        areaErro.appendChild(spanErro)
+        areaErro.appendChild(imgErro)
+    }
+}
+
+function removeErro(){
+    const error = document.querySelector('.error')
+    let erroImg = error.querySelector('img')
+    let erroSpan = error.querySelector('span')
+    if(error){
+        areaInput.classList.remove('error')
+        erroImg.remove()
+        erroSpan.remove()
+    }
+}
+
+btnEmail.addEventListener('click',checkEmail)
+
+
